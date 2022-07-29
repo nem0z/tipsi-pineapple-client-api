8e7d3b75c910f6f1aaa2c6136c8d417817df11ee
nem0z <maxime.bernard.dev@gmail.com>
2022-07-29 15:37:01 +0200

    building get and post method

--- Client.js
+++ Client.js
@@ -1,18 +1,45 @@
+import fetch from 'node-fetch';
+
 class Client {
     constructor(url = 'http://localhost:3000') {
         this.baseURL = url;
     }
 
-    get(id=null) {
-        const endpoint = `${this.baseURL}/${id ? `/product/${id}` : '/products'}`;
-        
-        return fetch(endpoint)
-            .then(res => res.json())
-            .catch(err => {
-                // manage error here
-                return err;
-            });
+    request(url, params = null) {
+        return fetch(url, params)
+        .then(res => res.json())
+        .catch(err => {
+            // manage error here
+            return err;
+        });
     }
+
+    getProducts(id = null) {
+        const endpoint = `${this.baseURL}/${id ? `product/${id}` : 'products'}`;
+        
+        return this.request(endpoint);
+    }
+
+    getOrders() {
+        const endpoint = `${this.baseURL}/orders`;
+        
+        return this.request(endpoint);
+    }
+
+    createOrder(order) {
+        const endpoint = `${this.baseURL}/order`;
+
+        const params = {
+            method: 'POST',
+            body: JSON.stringify(order),
+            headers: { "Content-Type": "application/json" }
+        };
+
+        return this.request(endpoint, params);
+    }
+
+
+
 }
 
 export default Client;
