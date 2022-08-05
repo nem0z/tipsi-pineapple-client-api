00a78fac233b8e49821935cbc576baa871a614b5
nem0z <maxime.bernard.dev@gmail.com>
2022-08-05 14:17:24 +0200

    Client working for node and web browser

--- index.test.js
+++ index.test.js
@@ -1,18 +1,10 @@
 import Client from './Client.js';
 
 const client = new Client();
-console.log(client);
-
-const newOrder = [
-    {
-        "model": "piPhone 12",
-        "desc": "iPhone 12 at a fair price"
-    },
-    {
-        "model": "piPhone 12 Mini",
-        "desc": "Small iPhone 12 at a fair price"
-    }
-];
 
 const products = await client.getProducts();
 console.log(products);
+
+const newOrder = [products[0], products[products.length - 1]];
+const createdOrder = await client.createOrder(newOrder);
+console.log(createdOrder);
